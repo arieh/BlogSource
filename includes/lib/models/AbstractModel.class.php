0b902468ad1a1366dafe45c9b21d317719fe78da
Arieh <Arieh@Arieh-Laptop>
2010-08-15 11:59:31 +0300

    reordered methods

--- includes/lib/models/AbstractModel.class.php
+++ includes/lib/models/AbstractModel.class.php
@@ -64,29 +64,85 @@ abstract class AbstractModel{
 		$this->setAction();
 	}
 	
-	/**
-	 * executes the model's logic
-	 * @access public
-	 */
-	public function execute(){
-		if ($this->checkPermission()==false){
-    		$this->setError('noPermission');
-    		return false;
-    	}
-		
-    	$action = $this->getAction();
-    	
-		if (!$action || !array_key_exists($action,$this->_actions))
-		  throw new AbstractModelException('No Valid Action Was Set');
-		
-		$action_method = $this->_actions[$this->getAction()];
-		$this->$action_method();
-	}
+    /**
+     * sets default getters and setters (getParamname(), setParamname())
+     *
+     * properties access by this method must be protected or public. if a property
+     * is an array, and it's name is plural, accessing it with singular form will pop its first
+     * variable. also, if that variable is an array, it will be passed as a model result.
+     *
+     * for get<ParamName>:
+     *  @param bool whether to return singular with pop or with shift (default to shift)
+     *
+     * for set<ParamName>:
+     *  if paramater is an array, all passed paramaters will be pushed
+     */
+    public function __call($name,$args){
+        $action = substr($name,0,3);
+        $pVar=array();
+        if ($action === 'get'){
+            $pVar = $this->explodeCase(substr($name,3));
+        
+        }elseif (substr($name,0,2)=='is'){
+            $pVar = $this->explodeCase(substr($name,2));
+            $action='is';
+        
+        }
+        
+        $pVar = "_".implode("_",$pVar);
+        $sVar = $pVar.'s';
+    
+        $pVarExists = (isset($this->$pVar) || property_exists($this,$pVar));
+        $sVarExists = (
+            (isset($this->$sVar) || property_exists($this,$sVar))
+            && is_array($this->$sVar)
+        );
+        
+        if (!$pVarExists && !$sVarExists) throw new LogicException("Call to undefiend Method: $name");
+    
+        switch ($action){
+            case 'get':
+                if ($sVarExists){
+                    $var = (isset($args[0]) && $args[0]) ? array_pop($this->$sVar) : array_shift($this->$sVar);
+                    if ($var){
+                         return $var;
+                    }
+                    return false;
+                }
+                return $this->$pVar;
+            break;
+            case 'is':
+                return (bool)($this->$pVar);
+            break;
+            default:
+             throw new LogicException("Call to undefiend Method: $name");
+            break;
+        }
+    }
 	
 	public function __destruct(){
   		if (isset($this->_db) && method_exists($this->_db,'__destruct')) $this->_db->__destruct();
   	}
-	
+    
+    /**
+     * executes the model's logic
+     * @access public
+     */
+    public function execute(){
+        if ($this->checkPermission()==false){
+            $this->setError('noPermission');
+            return false;
+        }
+        
+        $action = $this->getAction();
+        
+        if (!$action || !array_key_exists($action,$this->_actions))
+          throw new AbstractModelException('No Valid Action Was Set');
+        
+        $action_method = $this->_actions[$this->getAction()];
+        $this->$action_method();
+    }
+    
 	/**
 	 * returns a model paramater
 	 * 	@param string $name a parameter name
@@ -106,15 +162,6 @@ abstract class AbstractModel{
 	 */
 	public function setOption($name,$value){
 		$this->options[$name] = $value;
-	}
-	
-	/**
-	 * checks if a specific option is set (good when an option is expected to be boolean)
-	 * 	@param string $name option name
-	 * @return bool
-	 */
-	protected function isOptionSet($name){
-		return (array_key_exists($name,$this->options));
 	}
 	
 	/**
@@ -147,79 +194,6 @@ abstract class AbstractModel{
 	    if (!$this->_action) return $this->_default_action;
 	 	return $this->_action;
 	 }
-	 
-	 /**
-	  * get a permission from the permission list
-	  * @return int permission id
-	  * @access protected
-	  */
-	 protected function getPermission(){
-	 	if ($this->_permission_counter === count($this->_permissions)) return false;
-	 	return $this->_permissions[$this->_permission_counter++];
-	 }
-	 
-	 /**
-	  * gets permission list
-	  * @access protected
-	  * @return array
-	  */
-	 protected function getPermissions(){
-	 	return $this->_permissions;
-	 }
-	 
-	/**
-	 * checks if a specific user has permission to access the page
-	 * @return bool
-	 */
-	protected function checkPermission(){return true;}
-	
-	/**
-	 * sets an internal error
-	 * 	@param string $name error name
-	 * @access protected
-	 */
-	protected function setError($name,$value=true){
-		$this->_errors[$name] = $value;
-	}
-	
-	/**
-	 * unsets an internal error
-	 * 	@param string $name erro name to unset
-	 * @access protected
-	 */
-	protected function unsetError($name){
-		if (isset($this->_errors[$name])) unset($this->_errors[$name]);
-	}
-	
-	/**
-	 * checks if an error exists for the model.
-	 * if no name is specified, checks if any errors were set
-	 * 	@param string $name error name to check
-	 * @access public
-	 * @return bool
-	 */
-	public function isError($name=false){
-		if ($name) return (bool) (isset($this->_errors[$name]) && $this->_errors[$name]);
-		return (bool)($this->_errors);
-	}
-	
-	/**
-	 * returns an array of errors
-	 * @access public
-	 * @return array
-	 */
-	public function getErrors(){
-		return array_keys($this->_errors);
-	}
-	
-	/**
-	 * check if in debug mode
-	 * @access protected
-	 * @return bool
-	 */
-	 protected function isDebug(){
-	 	return ($this->_debug==true);
-	 }
 	
 	/**
 	 * returns a JSON representation of the object
@@ -229,63 +203,90 @@ abstract class AbstractModel{
 	public function toJSON(){return '{}';}
 
 	
-	/**
-	 * sets default getters and setters (getParamname(), setParamname())
-	 *
-	 * properties access by this method must be protected or public. if a property
-	 * is an array, and it's name is plural, accessing it with singular form will pop its first
-	 * variable. also, if that variable is an array, it will be passed as a model result.
-	 *
-	 * for get<ParamName>:
-	 * 	@param bool whether to return singular with pop or with shift (default to shift)
-	 *
-	 * for set<ParamName>:
-	 * 	if paramater is an array, all passed paramaters will be pushed
-	 */
-	public function __call($name,$args){
-	    $action = substr($name,0,3);
-		$pVar=array();
-		if ($action === 'get'){
-			$pVar = $this->explodeCase(substr($name,3));
-		
-		}elseif (substr($name,0,2)=='is'){
-			$pVar = $this->explodeCase(substr($name,2));
-			$action='is';
-		
-		}
-		
-		$pVar = "_".implode("_",$pVar);
-		$sVar = $pVar.'s';
-	
-		$pVarExists = (isset($this->$pVar) || property_exists($this,$pVar));
-		$sVarExists = (
-			(isset($this->$sVar) || property_exists($this,$sVar))
-			&& is_array($this->$sVar)
-		);
-		
-		if (!$pVarExists && !$sVarExists) throw new LogicException("Call to undefiend Method: $name");
-	
-		switch ($action){
-			case 'get':
-				if ($sVarExists){
-					$var = (isset($args[0]) && $args[0]) ? array_pop($this->$sVar) : array_shift($this->$sVar);
-					if ($var){
-						 return $var;
-					}
-					return false;
-				}
-				return $this->$pVar;
-			break;
-			case 'is':
-				return (bool)($this->$pVar);
-			break;
-			default:
-			 throw new LogicException("Call to undefiend Method: $name");
-			break;
-		}
-	}
-	
-	protected function explodeCase($string, $lower = true){
+    
+    /**
+     * checks if an error exists for the model.
+     * if no name is specified, checks if any errors were set
+     *  @param string $name error name to check
+     * @access public
+     * @return bool
+     */
+    public function isError($name=false){
+        if ($name) return (bool) (isset($this->_errors[$name]) && $this->_errors[$name]);
+        return (bool)($this->_errors);
+    }
+    
+    /**
+     * returns an array of errors
+     * @access public
+     * @return array
+     */
+    public function getErrors(){
+        return array_keys($this->_errors);
+    }
+     
+     /**
+      * get a permission from the permission list
+      * @return int permission id
+      * @access protected
+      */
+     protected function getPermission(){
+        if ($this->_permission_counter === count($this->_permissions)) return false;
+        return $this->_permissions[$this->_permission_counter++];
+     }
+     
+     /**
+      * gets permission list
+      * @access protected
+      * @return array
+      */
+     protected function getPermissions(){
+        return $this->_permissions;
+     }
+     
+    /**
+     * checks if a specific user has permission to access the page
+     * @return bool
+     */
+    protected function checkPermission(){return true;}
+    
+    /**
+     * sets an internal error
+     *  @param string $name error name
+     * @access protected
+     */
+    protected function setError($name,$value=true){
+        $this->_errors[$name] = $value;
+    }
+    
+    /**
+     * unsets an internal error
+     *  @param string $name erro name to unset
+     * @access protected
+     */
+    protected function unsetError($name){
+        if (isset($this->_errors[$name])) unset($this->_errors[$name]);
+    }
+    
+    /**
+     * checks if a specific option is set (good when an option is expected to be boolean)
+     *  @param string $name option name
+     * @return bool
+     */
+    protected function isOptionSet($name){
+        return (array_key_exists($name,$this->options));
+    }
+    
+    /**
+     * check if in debug mode
+     * @access protected
+     * @return bool
+     */
+     protected function isDebug(){
+        return ($this->_debug==true);
+     }
+    
+    protected function explodeCase($string, $lower = true){
       // Split up the string into an array according to the uppercase characters
       $array = preg_split('/([A-Z][^A-Z]*)/', $string, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
       
