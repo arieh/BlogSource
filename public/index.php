c93871b2c5614cd53b488929420ee4971f8a9c73
Arieh <Arieh@Arieh-Laptop>
2010-08-16 10:12:24 +0300

    fixes. moved to TFView

--- public/index.php
+++ public/index.php
@@ -13,18 +13,16 @@ $user = new User();
 
 $router = new Router($paths[1]);
 
-$savant = new Savant3();
-$savant->addPath('template','../includes/app/templates');
-$savant->assign('base_path',$paths[0]);
-$savant->assign('cdn',$paths[2]);
+$view = new TFView('../includes/app/templates/');
+$view->assign('base_path',$paths[0]);
+$view->assign('cdn',$paths[2]);
 
-$debug = false;
-
+$online = false;
 if ($router->getFolder(0) == 'rss'){
     $router->removeFolder(0);
-    $controller = new RSSController($router,$savant,new PancakeTF_PDOAccess(), $debug);
+    $controller = new RSSController($router,$view,new PancakeTF_PDOAccess(), $online);
 }else{
-    $controller = new HTMLController($router,$savant,new PancakeTF_PDOAccess(), $debug);
+	$controller = new HTMLController($router,$view,new PancakeTF_PDOAccess(), $online);
 }
 
 echo $controller->generate();
